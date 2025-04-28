3063317e4b5d3bf840652ea496bcb30f81c9189f
daekwon <pranludi@gmail.com>
2025-04-28 20:38:37 +0900

    dispatcher service functional

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.4.5"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.polarbookshop"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+extra["springCloudVersion"] = "2024.0.1"
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.cloud:spring-cloud-function-context")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.projectreactor:reactor-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/test/java/com/polarbookshop/dispatcherservice/DispatchingFunctionsIntegrationTests.java
@@ -0,0 +1,34 @@
+package com.polarbookshop.dispatcherservice;
+
+import java.util.function.Function;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.function.context.FunctionCatalog;
+import org.springframework.cloud.function.context.test.FunctionalSpringBootTest;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+@FunctionalSpringBootTest
+class DispatchingFunctionsIntegrationTests {
+
+    @Autowired
+    FunctionCatalog catalog;
+
+    @Test
+    void packAndLabelOrder() {
+        Function<OrderAcceptedMessage, Flux<OrderDispatchedMessage>> packAndLabel =
+            catalog.lookup(
+                Function.class,
+                "pack|label"
+            );
+        long orderId = 121;
+
+        StepVerifier
+            .create(packAndLabel.apply(new OrderAcceptedMessage(orderId)))
+            .expectNextMatches(
+                dispatchedOrder ->
+                    dispatchedOrder.equals(new OrderDispatchedMessage(orderId))
+            )
+            .verifyComplete();
+    }
+}
